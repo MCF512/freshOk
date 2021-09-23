182f987f748cc5d5d0ba13665315f8f336dc5132
MCF512 <egdem4enko@yandex.ru>
2021-09-23 23:31:03 +0300

    added products

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,6 @@ $(function () {
     nextArrow: '<img class="slick-arrow next" src="../images/icons/slick-arrow.svg">',
     prevArrow: '<img class="slick-arrow prev" src="../images/icons/slick-arrow.svg">'
   });
+
+  var mixer = mixitup('.top-products__content');
 });
