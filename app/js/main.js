c78654c112f1c5136dec24bb4f89cdac2f03079d
MCF512 <egdem4enko@yandex.ru>
2021-09-23 15:52:36 +0300

    added slider and about

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
 $(function () {
-
+  $('.top-slider__inner').slick({
+    arrows: true,
+    nextArrow: '<img class="slick-arrow next" src="../images/icons/slick-arrow.svg">',
+    prevArrow: '<img class="slick-arrow prev" src="../images/icons/slick-arrow.svg">'
+  });
 });
