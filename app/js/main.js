fc846b820fcac73cbb5806d4ca0c2a343b51e3bc
MCF512 <egdem4enko@yandex.ru>
2021-09-30 23:48:49 +0300

    added banners, promo, brands, news

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,17 @@ $(function () {
     prevArrow: '<button class="slider__arrow slider__arrow--prev"></button>'
   });
 
-  var mixer = mixitup('.carts__list');
+
+  var containerEl1 = document.querySelector('[data-ref="mix1"]');
+  var containerEl2 = document.querySelector('[data-ref="mix2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer1 = mixitup(containerEl2, config);
 });
+
