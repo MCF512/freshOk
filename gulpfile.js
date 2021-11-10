be7d8fe7544466ada0b6f4d48013b95cce56d8df
MCF512 <egdem4enko@yandex.ru>
2021-11-10 14:58:13 +0300

    rename

--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,21 @@ const browserSync = require('browser-sync').create();
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir: 'app/'
+      baseDir: 'docs/'
     },
     notify: false
   })
 };
 
 function styles() {
-  return src('app/scss/style.scss')
+  return src('docs/scss/style.scss')
     .pipe(scss({ outputStyle: 'compressed' }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
       grid: true
     }))
-    .pipe(dest('app/css'))
+    .pipe(dest('docs/css'))
     .pipe(browserSync.stream())
 };
 
@@ -36,16 +36,16 @@ function scripts() {
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
-    'app/js/main.js'
+    'docs/js/main.js'
   ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
-    .pipe(dest('app/js'))
+    .pipe(dest('docs/js'))
     .pipe(browserSync.stream())
 };
 
 function images() {
-  return src('app/images/**/*.*')
+  return src('docs/images/**/*.*')
     .pipe(imagemin([
       imagemin.gifsicle({ interlaced: true }),
       imagemin.mozjpeg({ quality: 75, progressive: true }),
@@ -62,10 +62,10 @@ function images() {
 
 function build() {
   return src([
-    'app/**/*.html',
-    'app/css/style.min.css',
-    'app/js/main.min.js'
-  ], { base: 'app' })
+    'docs/**/*.html',
+    'docs/css/style.min.css',
+    'docs/js/main.min.js'
+  ], { base: 'docs' })
     .pipe(dest('dist'))
 }
 
@@ -74,9 +74,9 @@ function cleanDist() {
 }
 
 function watching() {
-  watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/**/*.html']).on('change', browserSync.reload)
+  watch(['docs/scss/**/*.scss'], styles);
+  watch(['docs/js/**/*.js', '!docs/js/main.min.js'], scripts);
+  watch(['docs/**/*.html']).on('change', browserSync.reload)
 
 };
 
